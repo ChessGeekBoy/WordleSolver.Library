6cfc735460409283d6d9436bd72bc2dce90119b0
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-02-27 12:33:07 -0800

    Filtering code

---
+++ CharacterStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace WordleSolver.Library
+{
+    public enum CharacterStatus
+    {
+        Black = 0,
+        Yellow = 1,
+        Green = 2
+    }
+}
---
+++ WordleSolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text.Json;
+using System.IO;
+
+namespace WordleSolver.Library
+{
+    public class WordleSolver
+    {
+        public IEnumerable<string> Possibilities { get; set; }
+
+        public WordleSolver(string jsonFilePath)
+        {
+            string json = File.ReadAllText(jsonFilePath);
+            this.Possibilities = JsonSerializer.Deserialize<IEnumerable<string>>(json);
+        }
+
+        public IEnumerable<string> FilterPossibilities(Dictionary<int, CharacterStatus> response, string attempt)
+        {
+            for (int index = 0; index < attempt.Length; index++)
+            {
+                switch (response[index])
+                {
+                    case CharacterStatus.Black:
+                        this.Possibilities = from possibility in this.Possibilities
+                                             where !possibility.Contains(attempt[index])
+                                             select possibility;
+                        break;
+                    case CharacterStatus.Yellow:
+                        this.Possibilities = from possibility in this.Possibilities
+                                             where possibility.Contains(attempt[index]) 
+                                             && possibility[index] != attempt[index]
+                                             select possibility;
+                        break;
+                    case CharacterStatus.Green:
+                        this.Possibilities = from possibility in this.Possibilities
+                                             where possibility[index] == attempt[index]
+                                             select possibility;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            return this.Possibilities;
+        }
+    }
+}
